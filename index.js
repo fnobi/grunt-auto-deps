249a7dc5f537512cdd9ef441b15ea9a9da8b980e
Fujisawa Shin <fujisawa-shin@kayac.com>
2015-04-04 21:38:33 +0900

    Revert "Merge pull request #2 from fnobi/feature/gulp-plugin"
    This reverts commit ff42a46e8fc8999df2a952e4c445d41a72768a4a, reversing
    changes made to 970c004a71410eff5d7078ab104ae5a725f97f66.

--- index.js
+++
@@ -1,3 +0,0 @@
-var Varline = require(__dirname + '/lib/Varline');
-Varline.gulp = require(__dirname + '/lib/gulp');
-module.exports = Varline;
--- tasks/auto_deps.js
+++ tasks/auto_deps.js
@@ -1,52 +1,94 @@
-var path = require('path');
-var async = require('async');
-
-var Varline = require(__dirname + '/../lib/Varline');
-
-var TASK_NAME = 'auto_deps';
-var TASK_DESCRIPTION = 'resolve js dependency and concat automatically.';
-
 module.exports = function (grunt) {
+    var taskName        = 'auto_deps',
+        taskDescription = 'resolve js dependency and concat automatically.',
+
+        path = require('path'),
+        async = require('async'),
+        pathToConcated = require('../lib/pathToConcated'),
+        scriptMatchList = require('../lib/scriptMatchList'),
+        bowerPathList = require('../lib/bowerPathList');
+        _ = grunt.util._;
+
     grunt.file.defaultEncoding = 'utf8';
 
-    grunt.registerMultiTask(TASK_NAME, TASK_DESCRIPTION, function () {
-        var target = this.target;
-        var config = grunt.config(TASK_NAME)[target];
-        var scripts = config.scripts || [];
-        var dest = config.dest || 'js';
-        var done = this.async();
+    grunt.registerMultiTask(taskName, taskDescription, function () {
+        var target = this.target,
+            config = grunt.config(taskName)[target],
 
-        var varline = new Varline(config);
+            // config parameters
+            loadPath = config.loadPath || ['src/js/*.js', 'src/js/**/*.js'],
+            scripts = config.scripts || [],
+            locate = config.locate || {},
+            alias = config.alias || {},
+            ignoredNames = config.ignore || [],
+            forceddNames = config.forced || [],
+            dest = config.dest || 'js',
+            wrap = config.wrap ? true : false,
 
-        varline.loadBowerComponents(function (err) {
-            // bower componentsの取得に失敗しても、候補に追加されないだけなので、
-            // 全体の処理は気にせず進める
+            done = this.async();
 
-            async.each(scripts, function (name, next) {
+        var pathsForName = scriptMatchList({
+            loadPath: loadPath,
+            locate: locate
+        });
+
+        async.series([function (next) {
+            bowerPathList(process.cwd(), function (err, list) {
+                if (err) {
+                    // bower componentsの取得に失敗しても、候補に失敗するだけなので、
+                    // 全体の処理は気にせず進める
+                    return next();
+                }
+
+                for (var i in list) {
+                    pathsForName[i] = list[i];
+                }
+                next();
+            });
+        }, function (next) {
+            scripts.forEach(function (name) {
                 var destPath = path.join(dest, name + '.js');
-                varline.resolve(name, function (err, result) {
-                    if (err) {
-                        console.error('[error] %s', destPath);
-                        console.error(err.toString());
-                        next();
-                        return;
-                    }
-                    console.log('[success] %s', destPath);
-                    console.log(result.vars);
-                    grunt.file.write(destPath, result.source);
-                    next();
+
+                // console.log('[script: %s]', name);
+                // console.log(' - dest path: %s', destPath);
+
+                if (!pathsForName[name]) {
+                    console.error('%s is not found on src.', name);
+                    return;
+                }
+
+                var vars = pathToConcated({
+                    scriptName: name,
+                    pathsForName: pathsForName,
+                    ignoredNames: ignoredNames,
+                    alias: alias
                 });
-            }, done);            
+                var sources = [];
+
+                forceddNames.forEach(function (n) {
+                    vars.unshift(n);
+                });
+
+                console.log('[write] %s (%s)', destPath, vars.join(', '));
+
+                vars.forEach(function (name) {
+                    sources.push(grunt.file.read(pathsForName[name]));
+                });
+
+                if (wrap) {
+                    // console.log('[wrap]');
+                    sources.unshift('(function () {');
+                    sources.push('})();');
+                }
+
+                grunt.file.write(destPath, sources.join('\n'));
+            });
+            next();
+        }], function (err) {
+            if (err) {
+                throw new Error(err);
+            }
+            done();
         });
     });
 };
-
-
-
-
-
-
-
-
-
-
